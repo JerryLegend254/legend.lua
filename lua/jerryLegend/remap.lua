99ea2b7b5b16b0ef8d68a1839609b9ea2d62c5a5
JerryLegend254 <jerryokuto713@gmail.com>
2024-06-20 09:31:54 +0300

    added remap for autoimport golang pkgs

--- lua/jerryLegend/remap.lua
+++ lua/jerryLegend/remap.lua
@@ -46,3 +46,7 @@ vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+
+vim.keymap.set("n", "<leader>giu", function()
+    vim.cmd("%!goimports")
+end)
